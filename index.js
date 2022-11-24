565d87af4022c409d46812fac0ddcf4ea6475ffe
sarwar-asik <shasik2021@gmail.com>
2022-11-24 23:47:11 +0600

    post products

--- index.js
+++ index.js
@@ -23,25 +23,54 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const usersCollections = client.db("used-cars-ass12").collection("users");
-    const categoriesCollections = client.db("used-cars-ass12").collection("categories");
-
-
-
+    const productsCollections = client
+      .db("used-cars-ass12")
+      .collection("products");
+    const categoriesCollections = client
+      .db("used-cars-ass12")
+      .collection("categories");
 
     app.post("/users", async (req, res) => {
       const user = req.body;
       // console.log(user);
       const result = await usersCollections.insertOne(user);
-      res.send(result)
+      res.send(result);
     });
 
-    app.post('category',async(req,res)=>{
-      const category = req.body
-      const result = await categoriesCollections.insertOne(category)
+    app.post("/category", async (req, res) => {
+      const category = req.body;
+      const result = await categoriesCollections.insertOne(category);
+      // console.log(category);
+      res.send(result);
+    });
+    // get categories
+
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const categories = await categoriesCollections.find(query).toArray();
+      res.send(categories);
+    });
+
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const users = await usersCollections.find(query).toArray();
+      res.send(users);
+    });
+
+    app.post("/products", async (req, res) => {
+      const products = req.body;
+      console.log(products);
+      const result = await productsCollections.insertOne(products);
+      res.send(result)
+    });
+    app.get('/products/:name',async(req,res)=>{
+      const name = req.params.name;
+      const query = {category:name}
+      const result = await productsCollections.find(query).toArray()
+      res.send(result)
+
+
     })
-    
-
-
   } finally {
   }
 }
